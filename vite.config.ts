e877300d27df2e2a0fea0659d79cdf3177eb33d4
Cyril556 <64355640+Cyril556@users.noreply.github.com>
2026-03-24 18:23:20 +0300

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+    base: '/MCQ-X-PBQ-SIM/',
   server: {
     host: "::",
     port: 8080,
